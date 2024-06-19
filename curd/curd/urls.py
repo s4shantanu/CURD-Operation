2d9c0249e9b616bc83f021acdee8b7fc9da6629c
Shantanu Raut <rshantanu73@gmail.com>
2024-06-20 00:06:11 +0530

    #

--- curd/curd/urls.py
+++ curd/curd/urls.py
@@ -19,5 +19,7 @@ from django.urls import path
 from enroll import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.add_show, name="addandshow")
+    path('', views.add_show, name="addandshow"),
+    path('delete/<int:id>', views.delete_data, name="delete_data"),
+    path('<int:id>', views.update_data, name="update_data"),
 ]
--- curd/enroll/views.py
+++ curd/enroll/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .forms import Studentregistration
 from .models import User
 
 # Create your views here.
+
 
 def add_show(request):
     if request.method == 'POST':
@@ -11,10 +12,29 @@ def add_show(request):
             nm = fm.cleaned_data['name']
             em = fm.cleaned_data['email']
             pw = fm.cleaned_data['password']
-            reg = User(name=nm, email=em)
+            reg = User(name=nm, email=em, password=pw)
             reg.save()
             fm = Studentregistration()
     else:
         fm = Studentregistration()
     stud = User.objects.all()
     return render(request, 'enroll/addandshow.html', {'form':fm, 'stu':stud})
+
+# This is the code for delete data
+def delete_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
+
+# This is the code for update data
+def update_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        fm = Studentregistration(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+    else:
+        pi = User.objects.get(pk=id)
+        fm = Studentregistration(instance=pi)
+    return render(request, 'enroll/updatestudent.html', {'form':fm})
